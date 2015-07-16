2533a799ee0852fe8eee605d138ce8ecd51cd6bd
liujiantao <liu_jian_tao@qq.com>
2015-07-16 21:46:56 +0800

    Fix Bugs

--- admin/user.php
+++ admin/user.php
@@ -4,27 +4,33 @@
 	$user = htmlspecialchars(stripslashes($_POST['user']));
 	$pass = $_POST['pass'];
 	$pass = md5($pass);
-	$stmt = $dbh->query("SELECT id,username,password,flag FROM user WHERE username='$user'");
+	$username = htmlspecialchars(stripslashes($_POST["username"]));
+	$password = htmlspecialchars(stripslashes($_POST["password"]));
+	$password = md5($password);
 	
-	$arr = $stmt->fetch(PDO::FETCH_NUM);
 	if($user != "" && $pass != ""){
-		if($arr['2'] != $pass || $arr['3'] != "1"){
+		$stmt = $dbh->query("SELECT id,username,password,flag FROM user WHERE username='$user'");
+		$arr = $stmt->fetch(PDO::FETCH_NUM);
+		if($arr['2'] != $pass || $arr['3'] != "0"){
 			echo "can't"; 
 		}else{
-			$query = "INSERT INTO user (username,password,flag) VALUES (?,?,?)";	
+			
+			$query = "INSERT INTO user (username,password) VALUES (?,?)";	
 			$stmt = $dbh->prepare($query);
+			
 			$stmt->bindParam(1,$username,PDO::PARAM_STR);
 			$stmt->bindParam(2,$password,PDO::PARAM_STR);
 	
-			$username = htmlspecialchars(stripslashes($_POST["username"]));
-			$password = htmlspecialchars(stripslashes($_POST["password"]));
-			$password = md5($password);
-		
-			if($username != "" && $password != ""){
-				$stmt->execute();
-				echo "true";
-			}else{
-				echo "false";
+			$stmts = $dbh->query("SELECT id,username FROM user WHERE username='$username'");
+			$arrs = $stmts->fetch(PDO::FETCH_NUM);
+			
+			if($arrs['0'] == ""){			
+				if($username != "" && $password != ""){
+					$stmt->execute();
+					echo "true";
+				}else{
+					echo "false";
+				}
 			}
 		}
 	}else{
